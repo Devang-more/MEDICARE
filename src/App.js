ca4aa1f3096fd4708842c30105a3fb4f89606cab
Devang-more <87374124+Devang-more@users.noreply.github.com>
2021-10-21 10:57:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import "./App.css";
 import Topbar from "./components/Topbar";
 import LogInPage from "./components/LogInPage";
-// gdhjb
-//Vijeta Singh Yadav
+
 function App() {
   return (
     <div className="app">
