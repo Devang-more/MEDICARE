f9c53776674688e7948df80761a2a9ece796dced
Devang-more <devangmore9999@gmail.com>
2021-12-15 09:09:55 +0530

    devang

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Topbar from "./components/Topbar";
 import LogInPage from "./components/LogInPage";
 import Signup_P from "./components/Signup_P";
 import Signup_D from './components/Signup_D';
+import Home from "./components/home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -12,10 +13,10 @@ function App() {
       <Topbar />
       <Switch>
           <Route path="/loginpage" exact component={LogInPage} />
-          <Route path="/" exact component={Signup_D } />
+          <Route path="/Signup_D" exact component={Signup_D } />
           <Route path="/Signup_P" exact component={Signup_P} />
-          {/* <Route path="/about" exact component={About} /> */}
-          {/* <Route path="/contact" exact component={Contact} /> */}
+          {/* <Route path="/Appointment" exact component={Appointment} /> */}
+          <Route path="/" exact component={Home} />
         </Switch>
         
       </Router>
---
+++ src/components/home.js
@@ -0,0 +1,69 @@
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="homePage">
+      <div className="homePageForm">
+        <h2 className="homePage__heading">Welcome to MediCare</h2>
+        <form>
+          <div className="homeformInput">
+            <input type="Location" id="location" placeholder="Enter Location" />
+          {/* </div>
+          <div className="formInput"> */}
+            <input type="text" id="text" placeholder="Enter doctor or speciality" />
+          
+          <Button className="searchButton">Search</Button>
+          </div>
+        </form>
+      </div>
+      <div className="options1">
+            <div className="doctors">
+                <Link to="/">
+                <h2 className="h2heading">Doctors Near By</h2>
+                <h4>Find the best doctors near your location</h4>
+                </Link>
+            </div>
+            <div className="appointment">
+                <Link to="/">
+                <h2 className="h2heading">Book Appointments</h2>
+                <h4>Book an appointment for the doctors according to availability(online/offline)</h4>  
+                </Link>
+            </div>
+      </div>
+      <div className="options2">
+            <div className="video">
+                <Link to="/">
+                <h2 className="h2heading">Video/Audio Consult</h2>
+                <h4>Direct consult the doctor online through Audio/Video comms</h4>
+                    </Link>
+            </div>
+            <div className="speciality">
+                    <Link to="/">
+                <h2 className="h2heading">Specialities</h2>
+                <h4>Search the doctors according to their speciality</h4>
+            </Link>
+            </div>
+      </div>
+      <div className="options3">
+        <div className="reports">
+                <Link to="/">
+            <h2 className="h2heading">Reports</h2>
+            <h4>History of reports of pateint</h4>
+        </Link>
+        </div>
+        <div className="Pappointments">
+                <Link to="/">
+            <h2 className="h2heading">Past Appointments</h2>
+            <h4>You can check your previous appointments and Doctors here</h4>
+        </Link>
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default Home;
